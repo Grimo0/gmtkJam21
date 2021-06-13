466a87e5a3b9701378d423def239d96d75f4d4e3
Grimo0 <gregoir.andre@gmail.com>
2021-06-13 16:45:08 +0200

    Add hearts to units

--- src/en/Unit.hx
+++ src/en/Unit.hx
@@ -1,14 +1,52 @@
 package en;
 
+import h2d.Bitmap;
+
 class Unit extends Entity {
 	public var id(default, null) : String;
 
-	public var maxLife(default, null) : Int;
+	var hearts = new Array<h2d.Bitmap>();
+
+	public var maxLife(default, set) : Int;
+	public function set_maxLife(v : Int) {
+		if (v > maxLife) { // Add hearts
+			for (i in maxLife...v) {
+				var heart = new h2d.Bitmap(Assets.ui.getTile("Heart"), spr);
+				hearts.push(heart);
+			}
+		} else { // Remove hearts
+			for (i in v...hearts.length) {
+				hearts[i].remove();
+			}
+			hearts.resize(v);
+		}
+
+		if (hearts.length > 0) {
+			var space = -2;
+			var x = -hearts.length * .5 * (hearts[0].tile.width + space) + space;
+			var y = -spr.frameData.hei * .5 - hearts[0].tile.height;
+			var s = .3;
+			for (h in hearts) {
+				h.x = x;
+				h.y = y + s * hearts[0].tile.height;
+				x += hearts[0].tile.width + space;
+				s = -s;
+			}
+		}
+
+		return maxLife = v;
+	}
 	public var life(default, set) : Int;
 	public function set_life(v : Int) {
-		final maxHP = maxLife;
-		if (v > maxHP)
-			return life = Std.int(maxHP);
+		if (v > maxLife)
+			v = Std.int(maxLife);
+		// Gaining life
+		for (i in life...v) {
+			hearts[i].visible = true;
+		}
+		for (i in v...life) {
+			hearts[i].visible = false;
+		}
 		return life = v;
 	}
 
@@ -17,6 +55,7 @@ class Unit extends Entity {
 	public function new(id : String) {
 		super(Assets.entities);
 		this.id = id;
+		spr.set(id);
 
 		reset();
 	}
